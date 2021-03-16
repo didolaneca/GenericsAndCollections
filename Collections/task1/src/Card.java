23937ab1225cdafb074d90df2005b54e2f3d2c5b
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-03-16 17:15:33 -0500

    Added constructor and getters for the Card class

--- Collections/task1/src/Card.java
+++ Collections/task1/src/Card.java
@@ -3,4 +3,16 @@ public class Card <V,S>{
     private S suit;
 /* TODO
 Implement the constructor, getters and setters and toString */
+    public Card(V value, S suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public S getSuit() {
+        return suit;
+    }
 }
