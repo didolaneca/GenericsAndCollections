463fb82159026881b3a1304564e289b4ad0cd39a
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-03-16 12:40:58 -0500

    Completed the BingoChip class

--- Generics/task1/src/BingoChip.java
+++ Generics/task1/src/BingoChip.java
@@ -1,4 +1,16 @@
-public class BingoChip/* TODO 
+public class BingoChip<S,I> {/* TODO
 Implement this class.
 Use the instructions and check how it is used in Task.java*/
+    private S column;
+    private I row;
 
+    public BingoChip(S column, I row){
+        this.column = column;
+        this.row = row;
+    }
+
+    @Override
+    public String toString(){
+        return "Chip:" + column + "-" + row;
+    }
+}
